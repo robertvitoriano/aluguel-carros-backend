67c748a419948164facc1a4fad7f786c07b52d1e
robertvitoriano <robertvitorianodev@gmail.com>
2021-03-23 06:17:53 -0300

    configure vs code debug

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,12 @@ import { createCourse } from './routes'
 const app = express()
 
 
-app.get('/',createCourse)
+app.post('/courses',(request, response)=>{
+    const {name} = request.body
 
+    console.log('NAME', name)
+
+})
 app.listen(3000,()=>{
     console.log('My app is running')
 })
