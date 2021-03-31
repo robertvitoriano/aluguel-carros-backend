bbe2a87482305ea00b82403fb3c0ff7d6d53d555
robertvitoriano <robertvitorianodev@gmail.com>
2021-03-30 21:53:31 -0300

    add modules folder to segregates entities correctly

---
+++ src/modules/cars/models/Specification.ts
@@ -0,0 +1,23 @@
+import {v4 as uuidV4} from 'uuid'
+
+ class Specification {
+
+  name:String
+  description:String
+  created_at:Date
+  id?:String
+ 
+  constructor () {
+    if (!this.id){
+      this.id = uuidV4()
+    }
+
+  }
+
+
+
+
+}
+
+
+export { Specification }
