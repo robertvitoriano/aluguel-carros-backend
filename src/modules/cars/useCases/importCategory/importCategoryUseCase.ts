3cfb8723584a2c0089461e0713a85f0898688845
Roberty Vitoriano <robertvitoriano@gmail.com>
2021-05-11 17:48:29 -0300

    add categories import though csv

--- src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -1,10 +1,28 @@
+import fs from 'fs'
+import csvParse from 'csv-parse'
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
 class ImportCategoryUseCase {
 
+  constructor(private categoriesRepository:ICategoriesRepository){
 
-  execute(file:any){
+  }
 
-    console.log(file)
 
+  execute(file: Express.Multer.File): void {
+
+    const stream = fs.createReadStream(file.path)
+
+    const parseFile = csvParse()
+    stream.pipe(parseFile)
+
+    parseFile.on("data",async(line)=>{
+
+       this.categoriesRepository.create({
+         name:line[0],
+         description:line[1]
+       })
+       
+    })
   }
 
 
