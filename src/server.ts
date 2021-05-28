85e754573c1aca188348a5500ba0f9279913d6a9
Robert Vitoriano <robertvitorianodev@gmail.com>
2021-05-28 06:35:34 -0300

    add swagger.json

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import { categoriesRoutes } from './routes/categories.routes'
 import { router } from './routes'
- 
+import swaggerUi from  'swagger-ui-express'
+import swaggerConfig from "./swagger.json" 
 import morgan from 'morgan'
 const app = express()
 
 
 app.use(express.json())
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerConfig))
 app.use(morgan('common'))
 app.use(router)
 
