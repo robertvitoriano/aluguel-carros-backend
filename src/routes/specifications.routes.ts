27b18bb9321dc888f129ef12348df7aba5597902
Robert Vitoriano <robertvitoriano@gmail.com>
2021-07-31 07:03:56 -0300

    refactor: createSpecificationController

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -2,7 +2,6 @@ import { Request, Router } from 'express'
 import { SpecificationRepository} from '../modules/cars/repositories/implementations/SpecificationRepository'
 import createSpecificationController from './../modules/cars/useCases/createSpecification/'
 const specificationRoutes = Router()
-const specificationRepository = new SpecificationRepository()
 
 specificationRoutes.post("/", createSpecificationController().handle)
 
