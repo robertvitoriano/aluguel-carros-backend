18c02b73729f23737e8150bfbe4e03101fc7f9d5
Robert Vitoriano <robertvitoriano@gmail.com>
2021-07-31 07:03:30 -0300

    fix: createSpecificationController

--- src/modules/cars/useCases/createSpecification/index.ts
+++ src/modules/cars/useCases/createSpecification/index.ts
@@ -3,10 +3,11 @@ import {CreateSpecificationUseCase} from './CreateSpecificationUseCase'
 import {SpecificationRepository} from './../../repositories/implementations/SpecificationRepository'
 
 
+export default () =>{
+  const specificationRepository = new SpecificationRepository();
+  const createSpecificationUseCase = new CreateSpecificationUseCase(specificationRepository);
+  const createSpecificationController = new CreateSpecificationController(createSpecificationUseCase)
 
-const specificationRepository = new SpecificationRepository();
-const createSpecificationUseCase = new CreateSpecificationUseCase(specificationRepository);
-const createSpecificationController = new CreateSpecificationController(createSpecificationUseCase)
+  return createSpecificationController
 
-
-export { createSpecificationUseCase, createSpecificationController}
+}
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,10 @@
 import { Request, Router } from 'express'
 import { SpecificationRepository} from '../modules/cars/repositories/implementations/SpecificationRepository'
-import {createSpecificationController} from './../modules/cars/useCases/createSpecification/'
+import createSpecificationController from './../modules/cars/useCases/createSpecification/'
 const specificationRoutes = Router()
 const specificationRepository = new SpecificationRepository()
 
-specificationRoutes.post("/", createSpecificationController.handle)
+specificationRoutes.post("/", createSpecificationController().handle)
 
 
 specificationRoutes.get("/",)
